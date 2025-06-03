dcb3dfbf31a707d58eb4ce5141b1c4ee203e44da
Sravya <sravyapedapati@gmail.com>
2025-06-03 13:41:31 +0530

    Replace submodule backend with actual backend code

---
+++ backend/API/DataAccess/Context.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using System;
+
+namespace DataAccess
+{
+    public class Context(IConfiguration configuration) : DbContext
+    {
+        public IConfiguration Configuration { get; } = configuration;
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = Configuration.GetConnectionString("DB");
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 42));
+
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+    }
+}
